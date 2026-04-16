258eba926eadff42bc8fa24041cccb9cceaacd36
Lane Durham <wlaned@unc.edu>
2026-04-16 17:22:54 -0400

    Initial Commit

---
+++ project3reminders/project3remindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  project3remindersApp.swift
+//  project3reminders
+//
+//  Created by Lane Durham on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct project3remindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
